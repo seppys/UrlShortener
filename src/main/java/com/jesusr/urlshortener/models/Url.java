f3d845523d0ae18cc5bc29621a0e2cd6fb572ef5
seppys <jesusmanuelr018@gmail.com>
2024-07-22 15:23:52 +0200

    Add url repository

---
+++ src/main/java/com/jesusr/urlshortener/models/Url.java
@@ -0,0 +1,25 @@
+package com.jesusr.urlshortener.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Data
+@Table(name="urls")
+public class Url
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="long_url")
+    @URL
+    private String longUrl;
+
+    @Column(name="url_key")
+    private String urlKey;
+
+    @Column(name="click_counter")
+    private int clickCounter;
+}
---
+++ src/main/java/com/jesusr/urlshortener/repositories/IUrlRepository.java
@@ -0,0 +1,11 @@
+package com.jesusr.urlshortener.repositories;
+
+import com.jesusr.urlshortener.models.Url;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface IUrlRepository extends CrudRepository<Url, Long>
+{
+    Url findById(long id);
+    List<Url> findAll();
+}
