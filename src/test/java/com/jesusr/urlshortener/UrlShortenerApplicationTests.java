3cb9875600f48c869f63d3437635429bb88c2c14
seppys <jesusmanuelr018@gmail.com>
2024-07-22 13:50:22 +0200

    Project start

---
+++ src/test/java/com/jesusr/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jesusr.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
